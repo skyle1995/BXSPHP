c15919f5138feb483af37bfc23445cb9e9c52866
skyle1995 <39904228+skyle1995@users.noreply.github.com>
2022-03-01 20:58:33 +0800

    更新模块

--- module/compile.php
+++ module/compile.php
@@ -16,18 +16,18 @@ function rc4($data, $pwd) {
         $box[$i] = $i;
     }
     for ($j = $i = 0; $i < 256; $i++) {
-        $j             = ($j + $box[$i] + $key[$i]) % 256;
-        $tmp         = $box[$i];
+        $j       = ($j + $box[$i] + $key[$i]) % 256;
+        $tmp     = $box[$i];
         $box[$i] = $box[$j];
         $box[$j] = $tmp;
     }
     for ($a = $j = $i = 0; $i < $data_length; $i++) {
-        $a             = ($a + 1) % 256;
-        $j             = ($j + $box[$a]) % 256;
-        $tmp         = $box[$a];
+        $a       = ($a + 1) % 256;
+        $j       = ($j + $box[$a]) % 256;
+        $tmp     = $box[$a];
         $box[$a] = $box[$j];
         $box[$j] = $tmp;
-        $k             = $box[(($box[$a] + $box[$j]) % 256)];
+        $k       = $box[(($box[$a] + $box[$j]) % 256)];
         $cipher .= chr(ord($data[$i]) ^ $k);
     }
     return $cipher;
