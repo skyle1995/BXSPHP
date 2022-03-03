6fc06559796e871df97ab419c591d930ebc7ed95
skyle1995 <39904228+skyle1995@users.noreply.github.com>
2022-03-03 22:09:05 +0800

    优化代码

--- apps/admin/auto.php
+++ apps/admin/auto.php
@@ -1,14 +1,12 @@
 <?php
-// 设置加密密钥
-define('SYS_KEY', $conf['syskey']);
-
+// 判断登录信息
 if(isset($_COOKIE["admin_token"])){
     $session=md5($conf['admin_user'].$conf['admin_pwd'].$password_hash);
-    $admin_token=authcode(daddslashes($_COOKIE['admin_token']), 'DECODE', SYS_KEY);
+    $admin_token=authcode(daddslashes($_COOKIE['admin_token']), 'DECODE', SYSKEY);
     list($user, $sid, $expiretime) = explode("\t", $admin_token);
     if($user == $conf['admin_user'] && $session==$sid && $expiretime>time()) {
         $expiretime=time() + 604800;
-        $token=authcode("{$user}\t{$session}\t{$expiretime}", 'ENCODE', SYS_KEY);
+        $token=authcode("{$user}\t{$session}\t{$expiretime}", 'ENCODE', SYSKEY);
         setcookie("admin_token", $token, time() + 604800);
         $islogin=1;
     }
