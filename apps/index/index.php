d9228c189697c8822e35862e24688aafe0955f6d
skyle1995 <39904228+skyle1995@users.noreply.github.com>
2022-03-03 20:53:47 +0800

    修改模板

--- apps/index/index.php
+++ apps/index/index.php
@@ -1,3 +1,5 @@
 <?php
-    sysmsg("欢迎使用百晓生PHP简易框架！<br / >作者QQ：1132131911（非诚勿扰）","温馨提醒");
+    sysmsg("欢迎使用百晓生PHP框架系统！<br />
+    本项目已经在GitHub开源免费使用：<a href='https://github.com/skyle1995/BXSPHP' target='_blank'>BXSPHP</a><br />
+    作者QQ：1132131911（非诚勿扰）","温馨提醒");
 ?>
